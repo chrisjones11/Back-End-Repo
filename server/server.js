29e7463f626699c31bd39896304c1fce77c176e4
jonesyk28 <jonesyk28@gmail.com>
2017-10-25 00:15:08 +0100

    add linting to project and fix all linting errors

--- server/server.js
+++ server/server.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const app = express();
-// const APIKEY = process.env.Key;
-// const fs = require('fs');
-// const axios = require('axios');
 const { json } = require('body-parser');
-// const config = require('./config');
-// const db = config.DB[process.env.NODE_ENV] || process.env.DB;
+
 
 app.use(json());
 
