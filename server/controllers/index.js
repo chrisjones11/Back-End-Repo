d212b9fddf8b5293e7fbc98a4e55a469295957b9
jonesyk28 <jonesyk28@gmail.com>
2017-10-25 19:57:37 +0100

    implement fetchHome function

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,7 +1,28 @@
-let models = require("../models/models");
+let models = require('../models/models');
+
 
 function fetchHome(req, res) {
-  res.send("HELLO");
-}
+  let newsStories = models.NewsStory;
+  newsStories.find()
+    .then(function (docs) {
+      res.send({ 'news': docs });
+    })
+    .catch( err => console.log(err));
+} 
+
+// function fetchGame(req, res) {
+ 
+//   let gameFeeds = models.GameFeed;
+//   gameFeeds.find()
+//     .then(function (docs) {
+//       console.log(docs);
+//       res.send({ 'games': docs});
+//     });
+// }
+
+
+
+module.exports = { fetchHome };
+
 
 module.exports = { fetchHome };
