a93bc7c7fa7c72c878e0da089b9761c82934f639
Sunny <ssharda92@gmail.com>
2017-10-25 17:19:04 +0100

    set up server for api requests, tested homepage route with postman

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const app = require('./server/server');
-const PORT = require('./server/config').PORT[process.env.NODE_ENV];
+const app = require("./server/server");
+const PORT = require("./server/config").PORT.dev;
 
 app.listen(PORT, function() {
   console.log(`listening on port ${PORT}`);
--- server/server.js
+++ server/server.js
@@ -1,19 +1,23 @@
-const express = require('express');
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
+const router = require("./routes/index");
 const app = express();
-const { json } = require('body-parser');
-var config = require('./config');
-var db = config.DB[process.env.NODE_ENV] || process.env.DB;
+const { json } = require("body-parser");
+var config = require("./config");
+var db = config.DB.dev;
 mongoose.Promise = global.Promise;
 
-mongoose.connect(db, {useMongoClient: true})
-  .then(() => console.log('successfully connected to', db))
-  .catch(err => console.log('connection failed', err));
+mongoose
+  .connect(db, { useMongoClient: true })
+  .then(() => console.log("successfully connected to", db))
+  .catch(err => console.log("connection failed", err));
+
+app.get("/", function(req, res) {
+  res.send("Hello World");
+});
 
 app.use(json());
 
-app.get('/', function(req, res) {
-  res.send('Hello World');
-});
+app.use("/api", router);
 
 module.exports = app;
